5934005bd746ef5e0b5c7148e403d714f967fe9d
Churikov Nikita <nikita@MacBook-Pro-Churikov.local>
2018-02-05 00:35:02 +0300

    add vpn install

--- setup/install-stuff.sh
+++ setup/install-stuff.sh
@@ -1,3 +1,4 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew cask install iterm2
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+brew install sstp-client
